5f34fad00390b347eeeca4c7517b97cac5c091d2
Rosmer López <rosmerlopez99@gmail.com>
2023-04-28 00:19:03 -0400

    feat(plugins.util): added file that combines the specifications of the util plugins

---
+++ lua/rvim/plugins/util.lua
@@ -0,0 +1,186 @@
+return {
+  -- A high-performance color highlighter which has no external dependencies!.
+  {
+    "NvChad/nvim-colorizer.lua",
+    lazy = true,
+    opts = {
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = false, -- "Name" codes like Blue or blue
+        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        AARRGGBB = false, -- 0xAARRGGBB hex codes
+        rgb_fn = false, -- CSS rgb() and rgba() functions
+        hsl_fn = false, -- CSS hsl() and hsla() functions
+        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        -- Available modes for `mode`: foreground, background,  virtualtext
+        mode = "background", -- Set the display mode.
+        -- Available methods are false / true / "normal" / "lsp" / "both"
+        -- True is same as normal
+        tailwind = false, -- Enable tailwind colors
+        -- parsers can contain values used in |user_default_options|
+        sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
+        virtualtext = "■",
+        -- update color values even if buffer is not focused
+        -- example use: cmp_menu, cmp_docs
+        always_update = true,
+      },
+      -- all the sub-options of filetypes apply to buftypes
+      buftypes = {},
+    },
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+    end,
+    event = "BufReadPost",
+  },
+
+  -- Git integration: signs, hunk actions, blame, etc.
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "契" },
+        topdelete = { text = "契" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+      preview_config = {
+        -- Options passed to nvim_open_win
+        border = Config.border,
+      },
+    },
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+    end,
+    event = "BufReadPost",
+  },
+
+  -- A format runner written in Lua.
+  {
+    "mhartington/formatter.nvim",
+    dependencies = { "mason.nvim" },
+    config = function()
+      -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+      require("formatter").setup({
+        -- All formatter configurations are opt-in
+        filetype = {
+          -- Formatter configurations for filetype "lua" go here
+          -- and will be executed in order
+          lua = {
+            -- "formatter.filetypes.lua" defines default configurations for the
+            -- "lua" filetype
+            require("formatter.filetypes.lua").stylua,
+          },
+
+          python = {
+            require("formatter.filetypes.python").autopep8,
+          },
+
+          sh = {
+            require("formatter.filetypes.sh").shfmt,
+          },
+
+          yaml = {
+            require("formatter.filetypes.yaml").yamlfmt,
+          },
+
+          -- Use the special "*" filetype for defining formatter configurations on
+          -- any filetype
+          ["*"] = {
+            -- "formatter.filetypes.any" defines default configurations for any
+            -- filetype
+            require("formatter.filetypes.any").remove_trailing_whitespace,
+          },
+        },
+      })
+    end,
+    event = "BufReadPost",
+  },
+
+  -- Preview markdown in a webview window.
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    opts = {
+      auto_load = true, -- whether to automatically load preview when
+      -- entering another markdown buffer
+      close_on_bdelete = true, -- close preview window on buffer delete
+      syntax = true, -- enable syntax highlighting, affects performance
+      theme = "dark", -- 'dark' or 'light'
+      update_on_change = true,
+      app = { "firefox", "--new-window" }, -- 'webview', 'browser', string or a table of strings
+      -- explained below
+      filetype = { "markdown" }, -- list of filetypes to recognize as markdown
+      -- relevant if update_on_change is true
+      throttle_at = 200000, -- start throttling when file exceeds this
+      -- amount of bytes in size
+      throttle_time = "auto", -- minimum amount of time in milliseconds
+      -- that has to pass before starting new render
+    },
+    config = function(_, opts)
+      require("peek").setup(opts)
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+    cmd = "PeekOpen",
+  },
+
+  -- Clangd LSP extensions plugin.
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    opts = {
+      extensions = {
+        autoSetHints = true,
+        inlay_hints = {
+          only_current_line = false,
+          only_current_line_autocmd = "CursorHold",
+          show_parameter_hints = true,
+          parameter_hints_prefix = "<- ",
+          other_hints_prefix = "=> ",
+          max_len_align = false,
+          max_len_align_padding = 1,
+          right_align = false,
+          right_align_padding = 7,
+          highlight = "Comment",
+          priority = 100,
+        },
+        ast = {
+          role_icons = {
+            type = "",
+            declaration = "",
+            expression = "",
+            specifier = "",
+            statement = "",
+            ["template argument"] = "",
+          },
+          kind_icons = {
+            Compound = "",
+            Recovery = "",
+            TranslationUnit = "",
+            PackExpansion = "",
+            TemplateTypeParm = "",
+            TemplateTemplateParm = "",
+            TemplateParamObject = "",
+          },
+          highlights = {
+            detail = "Comment",
+          },
+        },
+        memory_usage = {
+          border = Config.border,
+        },
+        symbol_info = {
+          border = Config.border,
+        },
+      },
+    },
+    config = function(_, opts)
+      Config.servers.clangd = require("clangd_extensions").prepare(opts)
+    end,
+  },
+}
