1238d5ae14d7e0422b420804ef755636f68d53d9
Rosmer López <rosmerlopez99@gmail.com>
2023-04-28 00:20:53 -0400

    feat(plugins.coding): added file that combines the specifications of the coding plugins

---
+++ lua/rvim/plugins/coding.lua
@@ -0,0 +1,119 @@
+return {
+  -- A completion plugin written in Lua. New version of nvim-compe.
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = true,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.kind = string.format("%s %s", Config.icons.kinds[vim_item.kind], vim_item.kind)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+        window = {
+          completion = cmp.config.window.bordered({ border = Config.border }),
+          documentation = cmp.config.window.bordered({ border = Config.border }),
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+    event = "InsertEnter",
+  },
+
+  -- A snippet engine written in Lua.
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = true,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      updateevents = "TextChanged,TextChangedI",
+    },
+    config = function(_, opts)
+      require("luasnip").config.set_config(opts)
+    end,
+  },
+
+  -- A minimalist autopairs written by Lua.
+  {
+    "windwp/nvim-autopairs",
+    lazy = true,
+    opts = {
+      disable_filetype = { "TelescopePrompt", "vim" },
+    },
+    config = function(_, opts)
+      require("nvim-autopairs").setup(opts)
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      cmp.event:off("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+    event = "InsertEnter",
+  },
+}
