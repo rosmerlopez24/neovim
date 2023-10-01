ac8f2cca80ef69c43d0d72cc645a2006af532f8c
Rosmer López <rosmerlopez99@gmail.com>
2023-10-01 18:31:33 -0400

    refactor(core): updated process of setup

--- lua/rvim/core/init.lua
+++ lua/rvim/core/init.lua
@@ -1,10 +1,104 @@
----@type RVConfig
+---@class M
+---@type table
 local M = {}
 
----@type fun():nil
+---@class Config
+local defaults = {
+  -- General settings
+  -- Border Win
+  -- See :h nvim_open_win for possible border options
+  ---@type string
+  border = "rounded",
+  -- Colorscheme and style
+  -- Options: "catppuccin", "material", "nord", "onedark", "tokyonight"
+  ---@type string
+  colorscheme = "material",
+  -- Only applies to the following color schemes
+  -- catppuccin: latte, frappe, macchiato, mocha
+  -- material: darker, lighter, oceanic, palenight, deep ocean
+  -- tokyonight: storm, moon, night, day
+  -- bluloco: dark, light
+  ---@type string
+  style = "deep ocean",
+
+  -- Treesitter - Syntax highlighting
+  -- See https://github.com/nvim-treesitter/nvim-treesitter
+  ---@type table
+  plugins = {
+    treesitter = {
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = {},
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = false,
+      -- List of parsers to ignore installing (for "all")
+      ignore_install = {},
+    },
+  },
+
+  -- Icons for some plugins
+  ---@type table
+  icons = {
+    diagnostics = {
+      Error = " ",
+      Warn = " ",
+      Info = " ",
+      Hint = " ",
+    },
+    git = {
+      added = " ",
+      modified = " ",
+      removed = " ",
+    },
+    kinds = {
+      Array = " ",
+      Boolean = " ",
+      Class = " ",
+      Color = " ",
+      Constant = " ",
+      Constructor = " ",
+      Enum = " ",
+      EnumMember = " ",
+      Event = " ",
+      Field = " ",
+      File = " ",
+      Folder = " ",
+      Function = " ",
+      Interface = " ",
+      Key = " ",
+      Keyword = " ",
+      Method = " ",
+      Module = " ",
+      Namespace = " ",
+      Null = "ﳠ ",
+      Number = " ",
+      Object = " ",
+      Operator = " ",
+      Package = " ",
+      Property = " ",
+      Reference = " ",
+      Snippet = " ",
+      String = " ",
+      Struct = " ",
+      Text = " ",
+      TypeParameter = " ",
+      Unit = " ",
+      Value = " ",
+      Variable = " ",
+    },
+  },
+
+  -- LSP Server Settings
+  ---@type table
+  servers = {},
+}
+
+---@type function
 function M.setup()
-  ---@type RVConfig
-  Config = vim.deepcopy(require("rvim.config.defaults"))
+  ---@type Config
+  Config = vim.deepcopy(defaults)
 
   -- Try load user config based on default config
   local present, _ = pcall(require, "rvim.config")
