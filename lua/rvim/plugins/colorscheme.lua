1292adce03c2a73a1dbf920c2e018eecb6c81145
Rosmer López <rosmerlopez99@gmail.com>
2025-03-28 01:04:34 -0400

    refactor(colorscheme): minor changes

--- lua/rvim/plugins/colorscheme.lua
+++ lua/rvim/plugins/colorscheme.lua
@@ -8,21 +8,61 @@ return {
     lazy = RVimOptions.colorscheme ~= "material",
     opts = {
       contrast = {
-        terminal = false,
-        sidebars = true,
-        floating_windows = false,
-        cursor_line = false,
-        non_current_windows = false,
-        filetypes = {},
+        terminal = false, -- Enable contrast for the built-in terminal
+        sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = false, -- Enable contrast for floating windows
+        cursor_line = false, -- Enable darker background for the cursor line
+        lsp_virtual_text = false, -- Enable contrasted background for lsp virtual text
+        non_current_windows = false, -- Enable contrasted background for non-current windows
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
       },
-      plugins = {
+      plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        -- "coc",
+        -- "colorful-winsep",
+        -- "dap",
+        -- "dashboard",
+        -- "eyeliner",
+        -- "fidget",
+        -- "flash",
         "gitsigns",
+        -- "harpoon",
+        -- "hop",
+        -- "illuminate",
         "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        -- "neogit",
+        -- "neotest",
+        -- "neo-tree",
+        -- "neorg",
+        -- "noice",
         "nvim-cmp",
-        "nvim-navic",
+        -- "nvim-navic",
         "nvim-tree",
         "nvim-web-devicons",
+        -- "rainbow-delimiters",
+        -- "sneak",
+        -- "telescope",
+        -- "trouble",
+        -- "which-key",
+        -- "nvim-notify",
       },
+      disable = {
+        colored_cursor = false, -- Disable the colored cursor
+        borders = false, -- Disable borders between vertically split windows
+        background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+        term_colors = false, -- Prevent the theme from setting terminal colors
+        eob_lines = false, -- Hide the end-of-buffer lines
+      },
+      high_visibility = {
+        lighter = false, -- Enable higher contrast text for lighter style
+        darker = false, -- Enable higher contrast text for darker style
+      },
+      lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+      async_loading = true, -- Load parts of the theme asynchronously for faster startup (turned on by default)
+      custom_colors = nil, -- If you want to override the default colors, set this to a function
+      custom_highlights = {}, -- Overwrite highlights with your own
     },
     config = function(_, opts)
       require("material").setup(opts)
@@ -112,6 +152,7 @@ return {
       day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
       dim_inactive = false, -- dims inactive windows
       lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+      cache = true, -- When set to true, the theme will be cached for better performance
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
