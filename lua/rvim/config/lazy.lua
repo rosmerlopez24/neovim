a09ba77e021ceefb72c04feeb46a2f45e4dd6e79
Rosmer López <rosmerlopez99@gmail.com>
2025-04-03 03:47:00 -0400

    refactor(lazy): removed unnecesary code and minor changes

--- lua/rvim/config/lazy.lua
+++ lua/rvim/config/lazy.lua
@@ -22,18 +22,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup configurations
 require("lazy").setup({
-  root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
-  defaults = {
-    -- Set this to `true` to have all your plugins lazy-loaded by default.
-    -- Only do this if you know what you are doing, as it can lead to unexpected behavior.
-    lazy = false, -- should plugins be lazy-loaded?
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = nil, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-    -- default `cond` you can use to globally disable a lot of plugins
-    -- when running inside vscode for example
-  },
   -- leave nil when passing the spec as the first argument to setup()
   spec = {
     { import = "rvim.plugins.coding" },
@@ -44,84 +32,18 @@ require("lazy").setup({
     { import = "rvim.plugins.treesitter" },
     { import = "rvim.plugins.ui" },
   },
-  install = {
-    -- install missing plugins on startup. This doesn't increase startup time.
-    missing = true,
-    -- try to load one of these colorschemes when starting an installation during startup
-    colorscheme = { "habamax" },
-  },
-  pkg = {
-    enabled = true,
-    cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
-    -- the first package source that is found for a plugin will be used.
-    sources = {
-      "lazy",
-      "rockspec", -- will only be used when rocks.enabled is true
-      "packspec",
-    },
-  },
-  rocks = {
-    enabled = true,
-    root = vim.fn.stdpath("data") .. "/lazy-rocks",
-    server = "https://nvim-neorocks.github.io/rocks-binaries/",
-    -- use hererocks to install luarocks?
-    -- set to `nil` to use hererocks when luarocks is not found
-    -- set to `true` to always use hererocks
-    -- set to `false` to always use luarocks
-    hererocks = nil,
-  },
+  pkg = { enabled = true },
+  rocks = { enabled = false },
   ui = {
-    -- a number <1 is a percentage., >1 is a fixed size
-    size = { width = 0.8, height = 0.8 },
-    wrap = true, -- wrap the lines in the ui
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = RVimOptions.border,
-    -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
-    backdrop = 60,
-    title = nil, ---@type string only works when border is not "none"
-    title_pos = "center", ---@type "center" | "left" | "right"
-    -- Show pills on top of the Lazy window
-    pills = true, ---@type boolean
-    icons = {
-      cmd = " ",
-      config = "",
-      debug = "● ",
-      event = " ",
-      favorite = " ",
-      ft = "",
-      import = " ",
-      init = " ",
-      keys = " ",
-      lazy = "󰒲 ",
-      loaded = "",
-      not_loaded = "",
-      plugin = " ",
-      require = "󰢱 ",
-      runtime = " ",
-      source = " ",
-      start = " ",
-      task = "✔ ",
-      list = {
-        "●",
-        "➜",
-        "★",
-        "‒",
-      },
-    },
+    icons = { loaded = "", not_loaded = "" },
   },
   custom_keys = {},
-  checker = {
-    -- automatically check for plugin updates
-    enabled = false,
-    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
-    notify = true, -- get a notification when new updates are found
-    frequency = 3600, -- check for updates every hour
-    check_pinned = false, -- check for pinned packages that can't be updated
-  },
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = true,
-    notify = true, -- get a notification when changes are found
+    notify = false, -- get a notification when changes are found
   },
   performance = {
     cache = { enabled = true },
