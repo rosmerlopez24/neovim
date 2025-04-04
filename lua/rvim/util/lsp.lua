96d0644a4c2433d307e8dd79187b3c0616a63077
Rosmer López <rosmerlopez99@gmail.com>
2025-04-04 00:25:47 -0400

    refactor(util): rewrite module `lsp`

--- lua/rvim/util/lsp.lua
+++ lua/rvim/util/lsp.lua
@@ -1,118 +1,53 @@
 ---@class rvim.util.lsp
 local M = {}
 
----@type table<string, table<vim.lsp.Client, table<number, boolean>>>
-M._supports_method = {}
-
----@param buffer integer
-function M.default_mappings(buffer)
-  local opts = { noremap = true, silent = true, buffer = buffer }
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-end
-
 function M.setup()
-  local register_capability = vim.lsp.handlers["client/registerCapability"]
-  vim.lsp.handlers["client/registerCapability"] = function(err, res, ctx)
-    ---@diagnostic disable-next-line: no-unknown
-    local ret = register_capability(err, res, ctx)
-    local client = vim.lsp.get_client_by_id(ctx.client_id)
-    if client then
-      for buffer in pairs(client.attached_buffers) do
-        vim.api.nvim_exec_autocmds("User", {
-          pattern = "LspDynamicCapability",
-          data = { client_id = client.id, buffer = buffer },
-        })
-      end
-    end
-    return ret
-  end
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = RVimOptions.border,
-  })
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = RVimOptions.border,
-  })
-  M.on_attach(M._check_methods)
-  M.on_dynamic_capability(M._check_methods)
-end
+  -- Autocommand LspAttach
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("rvim-lsp-attach", {}),
+    callback = function(event)
+      local buffer = event.buf ---@type number
+      local client = vim.lsp.get_client_by_id(event.data.client_id)
 
----@param fn fun(client:vim.lsp.Client, buffer):boolean?
----@param opts? {group?: integer}
-function M.on_dynamic_capability(fn, opts)
-  return vim.api.nvim_create_autocmd("User", {
-    pattern = "LspDynamicCapability",
-    group = opts and opts.group or nil,
-    callback = function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      local buffer = args.data.buffer ---@type number
-      if client then
-        return fn(client, buffer)
+      -- Configure mappgins
+      local opts = { noremap = true, silent = true, buffer = buffer }
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      -- Enable document highlight
+      if client and client.server_capabilities.documentHighlightProvider then
+        local highlight_augroup = vim.api.nvim_create_augroup("rvim-lsp-highlight", {})
+
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          buffer = event.buf,
+          group = highlight_augroup,
+          callback = vim.lsp.buf.document_highlight,
+        })
+
+        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+          buffer = event.buf,
+          group = highlight_augroup,
+          callback = vim.lsp.buf.clear_references,
+        })
       end
     end,
   })
-end
 
----@param client vim.lsp.Client
-function M._check_methods(client, buffer)
-  -- don't trigger on invalid buffers
-  if not vim.api.nvim_buf_is_valid(buffer) then
-    return
-  end
-  -- don't trigger on non-listed buffers
-  if not vim.bo[buffer].buflisted then
-    return
-  end
-  -- don't trigger on nofile buffers
-  if vim.bo[buffer].buftype == "nofile" then
-    return
-  end
-  for method, clients in pairs(M._supports_method) do
-    clients[client] = clients[client] or {}
-    if not clients[client][buffer] then
-      if client.supports_method and client.supports_method(method, { bufnr = buffer }) then
-        clients[client][buffer] = true
-        vim.api.nvim_exec_autocmds("User", {
-          pattern = "LspSupportsMethod",
-          data = { client_id = client.id, buffer = buffer, method = method },
-        })
-      end
-    end
-  end
-end
-
----@param on_attach fun(client:vim.lsp.Client, buffer)
----@param name? string
-function M.on_attach(on_attach, name)
-  return vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-      local buffer = args.buf ---@type number
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if client and (not name or client.name == name) then
-        return on_attach(client, buffer)
-      end
-    end,
-  })
-end
-
----@param method string
----@param fn fun(client:vim.lsp.Client, buffer)
-function M.on_supports_method(method, fn)
-  M._supports_method[method] = M._supports_method[method] or setmetatable({}, { __mode = "k" })
-  return vim.api.nvim_create_autocmd("User", {
-    pattern = "LspSupportsMethod",
-    callback = function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      local buffer = args.data.buffer ---@type number
-      if client and method == args.data.method then
-        return fn(client, buffer)
-      end
+  -- Autocommand LspDetach
+  vim.api.nvim_create_autocmd("LspDetach", {
+    group = vim.api.nvim_create_augroup("rvim-lsp-detach", {}),
+    callback = function(event)
+      vim.lsp.buf.clear_references()
+      vim.api.nvim_clear_autocmds({
+        group = "rvim-lsp-detach",
+        buffer = event.buf,
+      })
     end,
   })
 end
