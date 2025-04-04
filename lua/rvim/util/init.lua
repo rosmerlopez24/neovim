78868e501d4ecabad7763e4bc8c2055c6647a858
Rosmer López <rosmerlopez99@gmail.com>
2025-04-03 22:52:54 -0400

    refactor(util): improvements and minor changes

--- lua/rvim/util/init.lua
+++ lua/rvim/util/init.lua
@@ -1,14 +1,21 @@
----@class RVimUtil
+---@class rvim.util
 ---@field cmp rvim.util.cmp
 ---@field lsp rvim.util.lsp
 ---@field ui rvim.util.ui
+---@field bufdelete rvim.util.bufdelete
 local M = {}
 
 setmetatable(M, {
   __index = function(table, key)
-    ---@diagnostic disable-next-line: no-unknown
-    table[key] = require("rvim.util." .. key)
-    return table[key]
+    local ok, module = pcall(require, "rvim.util." .. key)
+    if ok then
+      table[key] = module
+      return module
+    else
+      -- You can customize the error message according to your needs
+      error("Failed to load module: rvim.util." .. key .. "\n" .. module, 2)
+      return nil -- Or you could return another default value
+    end
   end,
 })
 
