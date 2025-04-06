2524cdafd14563401a71f9e84ba59d039af89da0
Rosmer López <rosmerlopez99@gmail.com>
2025-04-06 10:42:18 -0400

    refactor(colorsheme): rewrite `rose-pine` options

--- lua/rvim/plugins/colorscheme.lua
+++ lua/rvim/plugins/colorscheme.lua
@@ -189,58 +189,9 @@ return {
     lazy = RVimOptions.colorscheme ~= "rose-pine",
     priority = 1000,
     opts = {
-      --- @usage 'auto'|'main'|'moon'|'dawn'
-      variant = RVimOptions.style,
-      --- @usage 'main'|'moon'|'dawn'
-      dark_variant = RVimOptions.style,
-      bold_vert_split = false,
-      dim_nc_background = false,
-      disable_background = false,
-      disable_float_background = false,
-      disable_italics = true,
-
-      --- @usage string hex value or named color from rosepinetheme.com/palette
-      groups = {
-        background = "base",
-        background_nc = "_experimental_nc",
-        panel = "surface",
-        panel_nc = "base",
-        border = "highlight_med",
-        comment = "muted",
-        link = "iris",
-        punctuation = "subtle",
-
-        error = "love",
-        hint = "iris",
-        info = "foam",
-        warn = "gold",
-
-        headings = {
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
-        -- or set all headings at once
-        -- headings = 'subtle'
-      },
-
-      -- Change specific vim highlight groups
-      -- https://github.com/rose-pine/neovim/wiki/Recipes
-      highlight_groups = {
-        ColorColumn = { bg = "rose" },
-
-        -- Blend colours against the "base" background
-        CursorLine = { bg = "foam", blend = 10 },
-        StatusLine = { fg = "love", bg = "love", blend = 10 },
-
-        -- By default each group adds to the existing config.
-        -- If you only want to set what is written in this config exactly,
-        -- you can set the inherit option:
-        Search = { bg = "gold", inherit = false },
-      },
+      variant = RVimOptions.style, -- auto, main, moon, or dawn
+      dark_variant = RVimOptions.style, -- main, moon, or dawn
+      styles = { italic = false },
     },
     config = function(_, opts)
       require("rose-pine").setup(opts)
