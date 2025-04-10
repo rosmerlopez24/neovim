af90aa6cd41629a1c4bf978dcd5a38a0b101a483
Rosmer López <rosmerlopez99@gmail.com>
2025-04-09 22:23:37 -0400

    feat(editor): replace file explorer `nvim-tree.lua` to `oil.nvim`

--- lua/rvim/plugins/editor.lua
+++ lua/rvim/plugins/editor.lua
@@ -1,104 +1,17 @@
 return {
-  -- A simple and fast file explorer tree.
+
+  -- Neovim file explorer: edit your filesystem like a buffer
   {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = true,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-    keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<cr>" },
-    },
-    opts = {
-      hijack_cursor = true,
-      view = {
-        centralize_selection = false,
-        cursorline = true,
-        debounce_delay = 15,
-        number = false,
-        preserve_window_proportions = true,
-        relativenumber = false,
-        side = "left",
-        signcolumn = "yes",
-        width = 30,
-      },
-      renderer = {
-        add_trailing = false,
-        full_name = false,
-        group_empty = false,
-        hidden_display = "none",
-        highlight_bookmarks = "none",
-        highlight_clipboard = "name",
-        highlight_diagnostics = "none",
-        highlight_git = "none",
-        highlight_hidden = "none",
-        highlight_modified = "name",
-        highlight_opened_files = "name",
-        indent_width = 2,
-        root_folder_label = false,
-        symlink_destination = true,
-        indent_markers = {
-          enable = true,
-          inline_arrows = true,
-          icons = {
-            corner = "└",
-            edge = "│",
-            item = "│",
-            bottom = "─",
-            none = " ",
-          },
-        },
-        icons = {
-          bookmarks_placement = "signcolumn",
-          diagnostics_placement = "signcolumn",
-          git_placement = "before",
-          hidden_placement = "after",
-          modified_placement = "after",
-          padding = " ",
-          symlink_arrow = " ➛ ",
-          show = {
-            bookmarks = true,
-            diagnostics = false,
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-            hidden = false,
-            modified = true,
-          },
-          glyphs = {
-            bookmark = "󰆤",
-            default = "",
-            hidden = "󰜌",
-            modified = "●",
-            symlink = "",
-            folder = {
-              arrow_closed = "",
-              arrow_open = "",
-              default = "",
-              empty = "",
-              empty_open = "",
-              open = "",
-              symlink = "",
-              symlink_open = "",
-            },
-          },
-        },
-      },
-      git = { enable = false },
-      diagnostics = { enable = false },
-      modified = {
-        enable = true,
-        show_on_dirs = false,
-        show_on_open_dirs = false,
-      },
-      update_focused_file = {
-        enable = true,
-        update_root = false,
-      },
-    },
+    keys = { { "-", "<CMD>Oil<CR>", { desc = "Open parent directory" } } },
+    config = function(_, opts)
+      require("oil").setup(opts)
+    end,
   },
 
   -- Git integration: signs, hunk actions, blame, etc.
