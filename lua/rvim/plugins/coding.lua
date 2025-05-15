592fc98fa31140bd76ba8cae69d3e6cda893d132
Rosmer López <rosmerlopez99@gmail.com>
2025-05-15 03:17:09 -0400

    refactor(coding): improvements for `blink.cmp` config

--- lua/rvim/plugins/coding.lua
+++ lua/rvim/plugins/coding.lua
@@ -1,18 +1,13 @@
 return {
   -- blink.cmp is a completion plugin with support for LSPs and external sources
   -- that updates on every keystroke with minimal overhead (0.5-4ms async).
+  -- For more information visit: https://github.com/Saghen/blink.cmp
   {
     "saghen/blink.cmp",
     lazy = true,
     event = "BufReadPost",
-    -- optional: provides snippets for the snippet source
-    dependencies = { { "rafamadriz/friendly-snippets" } },
-    -- use a release tag to download pre-built binaries
+    dependencies = { "rafamadriz/friendly-snippets", "lazydev.nvim" },
     version = "1.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
@@ -30,24 +25,22 @@ return {
       -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = { preset = "enter" },
       appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = "mono",
         kind_icons = rvim.config.icons.kinds,
       },
-      -- (Default) Only show the documentation popup when manually triggered
       completion = {
         accept = {
-          -- experimental auto-brackets support
           auto_brackets = { enabled = true },
         },
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = {
+          auto_show = false,
+          auto_show_delay_ms = 500,
+          window = { border = rvim.config.border },
+        },
         menu = {
-          -- Keep the cursor X lines away from the top/bottom of the window
+          border = rvim.config.border,
           scrolloff = 2,
-          -- Note that the gutter will be disabled when border ~= 'none'
           scrollbar = false,
-          -- Controls how the completion items are rendered on the popup window
           draw = {
             treesitter = { "lsp" },
             columns = {
@@ -58,17 +51,14 @@ return {
           },
         },
       },
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        providers = {
+          lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+        },
       },
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
       fuzzy = { implementation = "prefer_rust_with_warning" },
+      signature = { enabled = true, window = { border = rvim.config.border } },
     },
     opts_extend = { "sources.default", "sources.completion.enabled_providers" },
   },
@@ -87,24 +77,6 @@ return {
         { path = "lazy.nvim", words = { "lazy" } },
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         { path = "rvim", words = { "rvim" } },
-      },
-    },
-  },
-
-  {
-    "saghen/blink.cmp",
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
       },
     },
   },
