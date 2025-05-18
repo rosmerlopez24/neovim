afdb99a1507498e71bae222824cbe8ba8f61f595
Rosmer López <rosmerlopez99@gmail.com>
2025-05-18 04:02:25 -0400

    refactor(colorscheme): rewrite `onedark` options

--- lua/rvim/plugins/colorscheme.lua
+++ lua/rvim/plugins/colorscheme.lua
@@ -162,60 +162,17 @@ return {
     end,
   },
 
-  -- For more information visit this Repo
-  -- https://github.com/navarasu/onedark.nvim
+  -- Atom's iconic One Dark theme for Neovim
+  -- https://github.com/olimorris/onedarkpro.nvim
   {
-    "navarasu/onedark.nvim",
+    "olimorris/onedarkpro.nvim",
     name = "onedark",
     lazy = rvim.config.colorscheme ~= "onedark",
     priority = 1000,
-    opts = {
-      -- Main options --
-      style = rvim.config.style, -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-      transparent = false, -- Show/hide background
-      term_colors = true, -- Change terminal color as per the selected theme style
-      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-      -- toggle theme style ---
-      toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-      toggle_style_list = {
-        "dark",
-        "darker",
-        "cool",
-        "deep",
-        "warm",
-        "warmer",
-        "light",
-      }, -- List of styles to toggle between
-
-      -- Change code style ---
-      -- Options are italic, bold, underline, none
-      -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-      code_style = {
-        comments = "italic",
-        keywords = "none",
-        functions = "none",
-        strings = "none",
-        variables = "none",
-      },
-
-      -- Lualine options --
-      lualine = {
-        transparent = false, -- lualine center bar transparency
-      },
-
-      -- Custom Highlights --
-      colors = {}, -- Override default colors
-      highlights = {}, -- Override highlight groups
-
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true, -- use undercurl instead of underline for diagnostics
-        background = true, -- use background color for virtual text
-      },
-    },
+    opts = {},
+    config = function(_, opts)
+      require("onedarkpro").setup(opts)
+    end,
   },
 
   -- All natural pine, faux fur and a bit of soho vibes for the classy minimalist
