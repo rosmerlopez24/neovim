a5587d684d560b85a8c3861ad8b9d637a6db8135
Rosmer López <rosmerlopez99@gmail.com>
2025-05-22 23:53:19 -0400

    refactor(ui): update specs for `lualine.nvim`

--- lua/rvim/plugins/ui.lua
+++ lua/rvim/plugins/ui.lua
@@ -5,82 +5,54 @@ return {
     lazy = true,
     event = "BufReadPost",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    init = function()
-      vim.g.lualine_laststatus = vim.o.laststatus
-      if vim.fn.argc(-1) > 0 then
-        -- set an empty statusline till lualine loads
-        vim.o.statusline = " "
-      else
-        -- hide the statusline on the starter page
-        vim.o.laststatus = 0
-      end
-    end,
     opts = {
       options = {
         icons_enabled = true,
         theme = rvim.config.colorscheme,
         section_separators = { left = "", right = "" },
         component_separators = { left = "", right = "" },
-        disabled_filetypes = {
-          statusline = { "NvimTree", "lazy" },
-        },
-        always_divide_middle = true,
         globalstatus = vim.o.laststatus == 3,
-        refresh = {
-          statusline = 100,
-          tabline = 100,
-          winbar = 100,
-        },
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = {
-          { "branch" },
+        lualine_b = { "branch" },
+        lualine_c = {
           {
             "diagnostics",
-            sources = { "nvim_lsp" },
-            sections = { "error", "warn", "info", "hint" },
-            diagnostics_color = {
-              error = "DiagnosticError",
-              warn = "DiagnosticWarn",
-              info = "DiagnosticInfo",
-              hint = "DiagnosticHint",
-            },
             symbols = {
               error = rvim.config.icons.diagnostics.Error,
               warn = rvim.config.icons.diagnostics.Warn,
               info = rvim.config.icons.diagnostics.Info,
               hint = rvim.config.icons.diagnostics.Hint,
             },
-            colored = true,
-            update_in_insert = true,
             always_visible = true,
           },
-        },
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-            symbols = { modified = "  ", readonly = "", unnamed = "" },
-          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 1, separator = "", padding = { left = 0, right = 1 } },
         },
         lualine_x = {
           {
             "diff",
-            colored = true,
             symbols = {
               added = rvim.config.icons.git.added,
               modified = rvim.config.icons.git.modified,
               removed = rvim.config.icons.git.removed,
             },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
           },
           { "encoding" },
           { "fileformat" },
-          { "filetype" },
         },
-        lualine_y = {
-          { "progress", separator = "", padding = { left = 1, right = 1 } },
-        },
+        lualine_y = { "progress" },
         lualine_z = { "location" },
       },
       inactive_sections = {
@@ -91,6 +63,7 @@ return {
         lualine_y = {},
         lualine_z = {},
       },
+      extensions = { "lazy", "oil" },
     },
     config = function(_, opts)
       require("lualine").setup(opts)
