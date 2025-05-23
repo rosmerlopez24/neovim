8ead6f4d35de118dfb34047f80910ef93730dcf5
Rosmer López <rosmerlopez99@gmail.com>
2025-05-22 23:53:55 -0400

    refactor(treesitter): update plugin specs

--- lua/rvim/plugins/treesitter.lua
+++ lua/rvim/plugins/treesitter.lua
@@ -4,15 +4,6 @@ return {
   build = ":TSUpdate",
   event = "BufReadPost",
   lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  init = function(plugin)
-    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-    -- Luckily, the only things that those plugins need are the custom queries, which we make available
-    -- during startup.
-    require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-  end,
   opts = {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = rvim.config.treesitter.ensure_installed,
